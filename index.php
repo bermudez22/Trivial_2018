2090d6241ee872592adf5ec22cbd2ce92174aed9
Administrator <Administrator@pc.localdomain>
2018-09-24 19:50:04 +0200

    Commit del Lunes 24-9
    con validación de formulario (jQuery Form Validator)

--- index.php
+++ index.php
@@ -6,15 +6,35 @@
         <title>PRUEBA DE PHP CON BOOTSTRAP</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     </head>
-    <body>
+    <body style="background-color:  #666">
         <div class="container">
             <div class ="row">
+                <div class="col-12">
+                    <h2 class="text-center" style="color:white;">EJEMPLO DE INICIO SESIÓN EN PHP</h2> 
+                </div>
+            </div>
+            <div class ="row">
                 <div class="col-4">
-                    <button id="boton1" class="btn btn-primary">Primary</button>
+                    
                 </div>
                 <div class="col-4">
-                    <input id ="cajaNombre" name="usuario_nombre" type="text" placeholder="Usuario">                </div>
-                <div class="col-4"></div>
+                    <br/><br/>
+                    <form action="index2.php">
+                        <input id ="cajaNombre" class="form-control" type="text" placeholder="Usuario" required="required">
+                        <br/>
+                        <input id ="cajaPassword" class="form-control" type="password" placeholder="Contraseña">
+                        <br/>
+                        <button id="boton1" class="btn btn-primary btn-block" type="submit">Primary</button>
+                        <br/><br/>
+                        <p>
+                            
+                        <input class="form-control" data-validation="date" data-validation-format="yyyy-mm-dd" placeholder="Year (yyyy-mm-dd):">
+                        </p>
+                    </form>
+                </div>
+                <div class="col-4">
+ 
+                </div>
             </div>
         </div>
         <?php
@@ -22,13 +42,18 @@
         ?>
     </body>
     <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
     
     <script>
          // document ready se ejecuta cuando toda la página se ha cargado correctamente
         $(document).ready(function(){
-            $('#cajaNombre').hide();
+            //$('#cajaNombre').hide();
         });
         
+        $.validate({
+          lang: 'es'
+         });
+  
         $('#boton1').click(function(){
             console.log("aqui si que llega");
             $('#cajaNombre').show(200);
