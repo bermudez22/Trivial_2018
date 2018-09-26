8077a5c8759ae53431c6b5c15c7c4c4ffc388fcb
Administrator <Administrator@pc.localdomain>
2018-09-26 16:24:49 +0200

    Miércoles 26/9
    añadiendo el inicio de sesión con php

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
         <title>PRUEBA DE PHP CON BOOTSTRAP</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        
     </head>
     <body style="background-color:  #666">
-        <div class="container">
+
+        <div class="container" id="principal">
             <div class ="row">
                 <div class="col-12">
                     <h2 class="text-center" style="color:white;">EJEMPLO DE INICIO SESIÓN EN PHP</h2> 
@@ -15,31 +18,20 @@
             </div>
             <div class ="row">
                 <div class="col-4">
-                    
                 </div>
                 <div class="col-4">
                     <br/><br/>
-                    <form action="index2.php">
                         <input id ="cajaNombre" class="form-control" type="text" placeholder="Usuario" required="required">
                         <br/>
                         <input id ="cajaPassword" class="form-control" type="password" placeholder="Contraseña">
                         <br/>
                         <button id="boton1" class="btn btn-primary btn-block" type="submit">Primary</button>
                         <br/><br/>
-                        <p>
-                            
-                        <input class="form-control" data-validation="date" data-validation-format="yyyy-mm-dd" placeholder="Year (yyyy-mm-dd):">
-                        </p>
-                    </form>
                 </div>
                 <div class="col-4">
- 
                 </div>
             </div>
         </div>
-        <?php
-        // put your code here
-        ?>
     </body>
     <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
@@ -55,8 +47,7 @@
          });
   
         $('#boton1').click(function(){
-            console.log("aqui si que llega");
-            $('#cajaNombre').show(200);
+            $('#principal').load("login.php");
         });
     </script>
 </html>
